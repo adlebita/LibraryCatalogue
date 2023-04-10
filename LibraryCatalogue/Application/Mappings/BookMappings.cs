d3b86fc10a51223c0e13272345ef915bd83e2002
LebitaA <adrian.lebita@ospri.co.nz>
2023-04-10 17:26:53 +1200

    Add Mapperly package, create book mappings

---
+++ LibraryCatalogue/Application/Mappings/BookMappings.cs
@@ -0,0 +1,11 @@
+﻿using LibraryCatalogue.Application.Commands.Books;
+using LibraryCatalogue.Application.Dtos.Requests;
+using Riok.Mapperly.Abstractions;
+
+namespace LibraryCatalogue.Application.Mappings;
+
+[Mapper]
+public partial class BookMappings
+{
+    public partial CreateBookCommand CreateBookRequestToCommand(CreateBookRequestDto createBookRequestDto);
+}
--- LibraryCatalogue/Program.cs
+++ LibraryCatalogue/Program.cs
@@ -1,7 +1,5 @@
-using LibraryCatalogue.Application.Commands.Books;
 using LibraryCatalogue.Application.Dtos.Requests;
-using LibraryCatalogue.Domain.Enums;
-using LibraryCatalogue.Domain.Models.Author;
+using LibraryCatalogue.Application.Mappings;
 using LibraryCatalogue.Infrastucture.Database;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
@@ -17,22 +15,12 @@ builder.Services.AddDbContext<LibraryContext>(options => options.UseInMemoryData
 
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
-
 app.MapGet("/catalogue", () => "List of Books");
 
-app.MapPost("/book", async (ISender sender, CreateBookRequestDto createBookRequestDto) =>
+app.MapPost("/books", async (ISender sender, CreateBookRequestDto createBookRequestDto) =>
 {
-    var author = Author.Create(createBookRequestDto.Author.FirstName,
-        createBookRequestDto.Author.LastName,
-        createBookRequestDto.Author.MiddleName,
-        createBookRequestDto.Author.BirthDate);
-
-    var createBookCommand = new CreateBookCommand(
-        createBookRequestDto.Title, 
-        author, 
-        createBookRequestDto.Description,
-        Enum.Parse<Genre>(createBookRequestDto.Genre));
+    var bookMapper = new BookMappings();
+    var createBookCommand = bookMapper.CreateBookRequestToCommand(createBookRequestDto);
     
     var bookId = await sender.Send(createBookCommand);
     return bookId;
