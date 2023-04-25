13b182a10b40318946c48db4519723441f0a0449
LebitaA <adrian.lebita@ospri.co.nz>
2023-04-25 14:51:40 +1200

    Create endpoints class and grouped book endpoints.

--- LibraryCatalogue/Program.cs
+++ LibraryCatalogue/Program.cs
@@ -1,10 +1,7 @@
-using LibraryCatalogue.Application.Commands.Books;
-using LibraryCatalogue.Application.Dtos.Requests;
+using LibraryCatalogue;
 using LibraryCatalogue.Application.Mappings;
-using LibraryCatalogue.Application.Queries;
 using LibraryCatalogue.Infrastructure.Database;
 using LibraryCatalogue.Infrastructure.Mediatr.PipelineBehaviour;
-using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,34 +26,6 @@ var app = builder.Build();
 
 app.UseHttpLogging();
 
-app.MapPost("/books", async (ISender sender, BookMappings bookMapper, CreateBookRequestDto createBookRequestDto) =>
-{
-    var createBookCommand = bookMapper.CreateBookRequestToCommand(createBookRequestDto);
-
-    var bookId = await sender.Send(createBookCommand);
-    return bookId;
-});
-
-app.MapGet("/books/{id:guid}", async (ISender sender, BookMappings bookMapper, Guid id) =>
-{
-    var book = await sender.Send(new GetBookByIdAsNoTracking(id));
-    if (book == null) return Results.NotFound();
-
-    var bookDto = bookMapper.CreateBookToBookDto(book);
-    return Results.Ok(bookDto);
-});
-
-app.MapDelete("/books/{id:guid}", async (ISender sender, Guid id) =>
-{
-    await sender.Send(new DeleteBookCommand(id));
-    return Results.NoContent();
-});
-
-app.MapPut("/books/{id:guid}", async (ISender sender, BookMappings bookMapper, Guid id, UpdateBookRequestDto updateBookRequestDto) =>
-{
-    var updateBookCommand = bookMapper.UpdateBookRequestToCommand(updateBookRequestDto) with { Id = id };
-    await sender.Send(updateBookCommand);
-    return Results.NoContent();
-});
+app.AddBooksEndpoints();
 
 app.Run();
