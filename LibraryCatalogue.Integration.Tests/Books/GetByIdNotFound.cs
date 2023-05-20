0fac1ffac25b09242821a5b654c4e023e499f6db
adlebita <lebita.adrian@gmail.com>
2023-05-20 19:36:04 +1200

    Implement integration test project and tests for create and get

---
+++ LibraryCatalogue.Integration.Tests/Books/GetByIdNotFound.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+#pragma warning disable CS8618
+
+namespace LibraryCatalogue.Integration.Tests.Books;
+
+public class GetByIdNotFound : IAsyncLifetime
+{
+    private WebApplicationFactory<Program> _webHost;
+    private HttpResponseMessage _response;
+
+    public async Task InitializeAsync()
+    {
+        _webHost = new WebApplicationFactory<Program>();
+
+        _response = await _webHost
+            .CreateClient()
+            .GetAsync($"/books/{Guid.NewGuid()}");
+    }
+
+    [Fact]
+    public void ShouldBe_NotFound() => Assert.Equal(HttpStatusCode.NotFound, _response.StatusCode);
+
+    public async Task DisposeAsync()
+    {
+        _response.Dispose();
+        await _webHost.DisposeAsync();
+    }
+}
--- LibraryCatalogue/Infrastructure/Mediatr/PipelineBehaviour/ValidationBehavior.cs
+++ LibraryCatalogue/Infrastructure/Mediatr/PipelineBehaviour/ValidationBehavior.cs
@@ -8,6 +8,13 @@ public record ValidationBehavior<TRequest, TResponse> : IPipelineBehavior<TReque
 {
     private readonly IValidator<TRequest> _validator;
 
+    // Todo: YOU MUST Rewrite this because an exception is thrown if a TRequest does not have a corresponding validator.
+    // The expectation is, if there is no validation matching TRequest then go straight through to next delegate.
+    // You have tried setting this to nullable does not work--- Idk why.
+    // Probably because the service provider looks for a corresponding IValidator and blows up when it can't find anything.
+    // I mean, think about it, how can you DI a NULLABLE object... idiot.
+    // I think the best approach may be to get scan or get list and iterate through each validation.
+    // 20/05/2023 @ 19:27
     public ValidationBehavior(IValidator<TRequest> validator) => _validator = validator;
 
     public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken ct)
